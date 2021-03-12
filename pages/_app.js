6c6d7464ba486745d0d6b5b5768348935d24574f
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-03-12 14:27:26 +0100

    changed og_image

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,10 @@ import { reduxWrapper, NextApp, MyHead } from 'eventjuicer-site-components';
 import Head from 'next/head'
 
 const MyCustomApp = (props) => <NextApp {...props} head={
-    <MyHead url="/">{(data) => <Head>{data}</Head>}</MyHead> 
+    <MyHead 
+        url="/" 
+        image="https://res.cloudinary.com/eventjuicer/image/upload/f_auto/v1615550856/virtualebe_reg.jpg"
+    >{(data) => <Head>{data}</Head>}</MyHead> 
 } />
 
 export default reduxWrapper.withRedux(MyCustomApp)
