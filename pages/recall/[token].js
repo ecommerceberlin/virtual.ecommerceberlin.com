f641b7b1580dd5160dbf7a9eaca80002e72eee1e
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-05-22 13:39:11 +0200

    https://github.com/ecommerceberlin/virtual.ecommerceberlin.com/issues/58

---
+++ pages/recall/[token].js
@@ -0,0 +1,64 @@
+import React, {useEffect} from 'react'
+import {
+    connect,
+    reduxWrapper,
+    configure,
+    getUserByToken,
+    Wrapper,
+    Loading,
+    useDispatch,
+    setUserToken
+  } from 'eventjuicer-site-components';
+  
+import {useRouter} from 'next/router'
+
+  import settings from '../../settings';
+  
+  const Recall = ({token, goto}) => {
+
+    const router = useRouter();
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+    
+        if(token){
+            dispatch(setUserToken(token))
+            router.push(goto)
+        }
+    })
+
+     return  (
+     <Wrapper style={{paddingTop: 100, paddingBottom: "80vh"}}>
+         {token ? <Loading /> : <h1>Error. Bad URL.</h1>}
+    </Wrapper>
+    )
+
+  }
+
+Recall.disableLayout = true;
+
+export const getServerSideProps = reduxWrapper.getServerSideProps(async (props) => {
+
+    const {query: {token = "", goto = "/stages/a"}} = props;
+
+    //const user = await getUserByToken(settings.system.service_api, token)
+
+    //action when userrrrr is bad?
+
+    await configure(props, {
+        settings: settings
+    })
+    return {
+        props: {
+            token,
+            goto
+        },
+    }  
+})
+  
+  
+export default connect()(Recall);
+  
+  
+  
+  
